75ce4b16883295bc1e9fcbf352b59acd2bde886e
Leon Si <contact@leonzalion.com>
2022-05-07 20:21:51 -0400

    feat: do not exit with 1

--- packages/run-if-env/src/bin/run-if-env.ts
+++ packages/run-if-env/src/bin/run-if-env.ts
@@ -1,14 +1,30 @@
 #!/usr/bin/env node
 
-import process from 'node:process'
-import qs from 'qs'
+import process from 'node:process';
+import qs from 'qs';
+import { execaCommandSync } from 'execa';
 
-const conditions = process.argv.slice(2);
-const query = conditions.join("&");
+const args = process.argv.slice(2);
+
+const scriptStartIndex = args.findIndex((arg) => !arg.includes('='));
+
+if (scriptStartIndex === -1) {
+	throw new Error('No script specified.');
+}
+
+const conditions = args.slice(0, scriptStartIndex);
+const script = args.slice(scriptStartIndex).join(' ');
+
+const query = conditions.join('&');
 const expected = qs.parse(query);
 
 for (const env in expected) {
-  if (process.env[env] !== expected[env]) {
-    process.exit(1);
-  }
+	if (process.env[env] !== expected[env]) {
+		console.info(
+			`run-if-env: Skipped running \`${script}\` because ${env} was equal to ${expected[env]}`
+		);
+		process.exit(0);
+	}
 }
+
+execaCommandSync(script, { shell: true, stdio: 'inherit' });
